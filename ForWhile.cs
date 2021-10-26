b7ce0e597ece8896b83d6f66fa2f026b465250ef
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-10-26 22:00:52 +0700

    For - While

---
+++ ForWhile.cs
@@ -0,0 +1,34 @@
+namespace CS002
+{
+    public class ForWhile
+    {
+        static void Main()
+        {
+            int i;
+            for (i = 0, System.Console.WriteLine("Start"); i < 5; i++, System.Console.WriteLine("Update i"))
+            {
+                System.Console.WriteLine(i);
+            }
+            // Start
+            // 0
+            // Update i
+            // 1
+            // Update i
+            // 2
+            // Update i
+            // 3
+            // Update i
+            // 4
+            // Update i
+
+            int n = 0;
+            while (n < 10)
+            {
+                n++;
+                // if (n == 5) break; // 1 2 3 4
+                if (n == 5) continue; // 1 2 3 4 6 7 8 9 10
+                System.Console.WriteLine(n);
+            }
+        }
+    }
+}
